71a4f75eb6e024cb4197e471ae41671904737f01
dundermain <sachin68834@gmail.com>
2024-10-02 13:17:10 +0530

    added database

---
+++ database/Categories.sql
@@ -0,0 +1,60 @@
+ /* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS TABLE_NAME(
+CategoryID	CategoryName	Description VARCHAR( 100 )
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '1  	Beverages  	Soft drinks',' coffees',' teas',' beers',' and ales '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '2  	Condiments  	Sweet and savory sauces',' relishes',' spreads',' and seasonings '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '3  	Confections  	Desserts',' candies',' and sweet breads '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '4  	Dairy Products  	Cheeses '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '5  	Grains/Cereals  	Breads',' crackers',' pasta',' and cereal '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '6  	Meat/Poultry  	Prepared meats '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '7  	Produce  	Dried fruit and bean curd '
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( CategoryID	CategoryName	Description )
+VALUES
+(
+    '8  	Seafood  	Seaweed and fish '
+);
